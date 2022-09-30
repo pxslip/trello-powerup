ada805360455a098503f84a95809d3710acf0e97
Will K (pxslip) <dev@pxslip.com>
2022-09-30 19:41:31 +0000

    Set the base url during deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
   ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL ?? '/',
+  },
 });

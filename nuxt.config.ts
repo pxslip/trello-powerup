86bf18534465be099ed2a1fd9281fd9b6c2b8d76
Will K (pxslip) <dev@pxslip.com>
2022-09-30 19:55:55 +0000

    The plugin expects the trello script to be there

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 import { defineNuxtConfig } from 'nuxt/config';
-console.log(process.env.NUXT_APP_BASE_URL);
 
 export default defineNuxtConfig({
-  ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
   app: {
     baseURL: process.env.NUXT_APP_BASE_URL ?? '/',

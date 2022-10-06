1d993814da6367b483091c8fd9ca07fc73f914e4
Will K (pxslip) <dev@pxslip.com>
2022-10-06 17:15:45 +0000

    Try this to better organize the code

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/trello-powerups/',
+  root: './src/',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        settings: resolve(__dirname, 'settings.html'),
+        main: resolve(__dirname, 'src', 'index.html'),
+        settings: resolve(__dirname, 'src', 'settings', 'settings.html'),
       },
     },
   },
